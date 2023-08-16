cb6fe24c5b71f231d258f93080d6ec13fe44296c
imkarthickperumal <karthickperumal.vp@gmail.com>
2023-08-16 20:47:14 +0530

    Redux CRUD APP | Create Operation working using useDispatch tasksadd

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { addTaskToList } from "../slices/tasksSlice";
+import { useDispatch } from "react-redux";
 
 const AddTask = () => {
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
   const addTask = (e) => {
     e.preventDefault();
     console.log({ title, description });
+    dispatch(addTaskToList({ title, description }));
+    setTitle("");
+    setDescription("");
   };
   return (
     <section className="my-5">
